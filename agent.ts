625afe6c93ec302a46d680cc1e64c204043256c1
Sahil <sahilsharma2000dtc@gmail.com>
2025-07-12 22:14:40 +0530

    tools : Get total expense

--- agent.ts
+++ agent.ts
@@ -1,4 +1,5 @@
 import Groq from 'groq-sdk';
+import {getTotalExpense} from './tools/getExpense';
 
 const groq = new Groq({apiKey: process.env.GROQ_API_KEY});
 
@@ -6,11 +7,40 @@ async function callAgent(){
     const completion = await groq.chat.completions.create({
         messages: [
             {
+                role: "system",
+                content: "You are Martin, a personal finance assistant. Your task is to assist the users to track their expenses, balances and financial planning",
+            },
+            {
                 role: "user",
-                content: "Explain the impotance of LLM"
+                content: "Explain the impotance of Ai Agent",
             },
         ],
-        model: "llama-3.3-70b-versatile"
+        model: "llama-3.3-70b-versatile",
+        tools: [
+            {
+                type: "function",
+                function: {
+                    name: 'getTotalExpense',
+                    description: "Get total expense from date to date.",
+                    parameters: {
+                        type: "object",
+                        propeties: {
+                            from: {
+                                type: "string",
+                                description:"From date to get the expense"
+                            },
+                            to: {
+                                type: "string",
+                                description: "To date to get the expense"
+                            }
+                        }
+                    }
+                }
+            }
+        ],
     });
-    
+
+    console.log( JSON.stringify(completion.choices[0].message , null, 2));
 }
+
+callAgent();
---
+++ tools/getExpense.ts
@@ -0,0 +1,16 @@
+/**
+ * AI agen tool
+ * -> Get TOTAL expense
+ */
+
+type TotalExpense = {
+    from : string | any;
+    to: string | any;
+}
+
+export function getTotalExpense({from, to}: TotalExpense){
+    console.log("Calling totoal expense tool")
+
+    // we have to call Database
+    return 10000;
+}
