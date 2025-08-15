459a0184f99f14c6ceda37041c69fb4ef6ed124b
Sahil <sahilsharma2000dtc@gmail.com>
2025-08-15 21:58:40 +0530

    fix: bug fix in database file and updated gitignore file

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,9 @@
-import {Database} from 'bun:sqlite';
+import { Database } from "bun:sqlite";
 
-export const db = new Database("finance.db");
+export const db = new Database("./db/finance.db");
 
 const createExpenseTable = `
-    CREATE TABLE IF NOT EXIST expenses (
+    CREATE TABLE IF NOT EXISTS expenses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     amount REAL NOT NULL,
@@ -12,7 +12,7 @@ const createExpenseTable = `
 `;
 
 const createIncomeTable = `
-    CREATE TABLE IF NOT EXIST income (
+    CREATE TABLE IF NOT EXISTS income (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     amount REAL NOT NULL,
@@ -20,75 +20,81 @@ const createIncomeTable = `
     );
 `;
 
-
-// initilise database creation 
-export function initDB(){
-     db.exec(createExpenseTable);
-     db.exec(createIncomeTable);
-     console.log("Database Initialized with the table.")
+// initilise database creation
+export function initDB() {
+  db.exec(createExpenseTable);
+  db.exec(createIncomeTable);
+  console.log("Database Initialized with the table.");
 }
-
 
 // Database functions to perform operations
 
 type AddAmount = {
-    name: string;
-    amount: string | number;
-}
-export async function addExpenseDB({name, amount}: AddAmount){
-    try {
-        if(!name && !amount) return;
+  name: string;
+  amount: string | number;
+};
+export async function addExpenseDB({ name, amount }: AddAmount) {
+  try {
+    if (!name && !amount) return;
 
-        db.run('INSERT INTO expenses (name, amout) VALUES (?, ?)', [name, amount])
-        return "expense added to the DB."
-    } catch (err) {
-        console.error("Error", err);
-    }
-}
-
-export async function addIncomeDB({name, amount}: {name: string, amount: number | string}){
-    try {
-        db.run('INSERT INTO income (name, amount) VALUES (?, ?)', [name, amount]);
-        return 'Income is added to the Income DB.'
-    } catch (err) {
-        console.error("Error: ", err)
-    }
+    db.run("INSERT INTO expenses (name, amount) VALUES (?, ?)", [name, amount]);
+    return "expense added to the DB.";
+  } catch (err) {
+    console.error("Error", err);
+  }
 }
 
-export async function getMoneyBalanceDB(){
-    try {
-        const incomeResult = db.query('SELECT SUM(amount) as totalIncome FROM income').get() as {totalIncome: number | null};
-        const expenseResult = db.query('SELECT SUM(amount) as totalExpense FROM expenses').get() as {totalExpense: number | null};
+export async function addIncomeDB({
+  name,
+  amount,
+}: {
+  name: string;
+  amount: number | string;
+}) {
+  try {
+    db.run("INSERT INTO income (name, amount) VALUES (?, ?)", [name, amount]);
+    return "Income is added to the Income DB.";
+  } catch (err) {
+    console.error("Error: ", err);
+  }
+}
 
-        const totalIncome = incomeResult?.totalIncome || 0;
-        const totalExpense = expenseResult?.totalExpense || 0;
+export async function getMoneyBalanceDB() {
+  try {
+    const incomeResult = db
+      .query("SELECT SUM(amount) as totalIncome FROM income")
+      .get() as { totalIncome: number | null };
+    const expenseResult = db
+      .query("SELECT SUM(amount) as totalExpense FROM expenses")
+      .get() as { totalExpense: number | null };
 
-        return `Current balance is ${(totalIncome - totalExpense).toFixed(2)} INR`
-    } catch (err) {
-        console.error("Error: ", err);
-    }
+    const totalIncome = incomeResult?.totalIncome || 0;
+    const totalExpense = expenseResult?.totalExpense || 0;
+
+    return `Current balance is ${(totalIncome - totalExpense).toFixed(2)} INR`;
+  } catch (err) {
+    console.error("Error: ", err);
+  }
 }
 
 export async function getAllExpenseDB() {
-    try {
-        const results = db.query('SELECT * FROM expenses ORDER BY date DESC').all();
-        return results;
-    } catch (err) {
-        console.error("Error ", err)
-        return [];
-    }
+  try {
+    const results = db.query("SELECT * FROM expenses ORDER BY date DESC").all();
+    return results;
+  } catch (err) {
+    console.error("Error ", err);
+    return [];
+  }
 }
-
 
 export async function getAllIncome() {
-    try {
-        const result = db.query('SELECT * FROM income ORDER BY date DESC').all();
-        return result;
-    } catch (err) {
-        console.error("Error ", err)
-        return [];
-    }
+  try {
+    const result = db.query("SELECT * FROM income ORDER BY date DESC").all();
+    return result;
+  } catch (err) {
+    console.error("Error ", err);
+    return [];
+  }
 }
-
 
 initDB();
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import {Elysia} from 'elysia';
+import { Elysia } from "elysia";
+import { addExpenseDB } from "./database";
 
 const app = new Elysia()
-    .get("/", () => "Application is running")
-    .get("/health", () => ({status: 200, message: "Server is healthy"}))
-    .listen(3001)
+  .get("/", () => "Finance Ai agent is running...")
+  .get("/health", () => ({ status: 200, message: "Server is healthy" }))
+  .post(
+    "/api/expense",
+    ({ body }: { body: { name: string; amount: number | string } }) => {
+      const result = addExpenseDB(body);
+      return { success: true, message: result };
+    }
+  )
+  .listen(3001);
 
-
-console.log(`🦊 Finance AI Agent API running at http://localhost:3001`)
+console.log(`🦊 Finance AI Agent API running at http://localhost:3001`);
